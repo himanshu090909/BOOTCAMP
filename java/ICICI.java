8e5b15761af8c4d8699ea267606b630eeb1729aa
himanshu090909 <60640645+himanshu090909@users.noreply.github.com>
2020-03-12 17:08:51 +0530

    Delete ICICI.java

--- java/ICICI.java
+++
@@ -1,8 +0,0 @@
-public class ICICI implements Bank
-{
-    @Override
-    public void getDetails() {
-        System.out.println("rate of interest is 8%");
-        System.out.println("education loan not available");
-    }
-}
