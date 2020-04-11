a525fceacda2e5d1801edffa61472eecc582aef4
himanshu090909 <himanshu.bhansali@tothenew.com>
2020-04-11 20:51:04 +0530

    ecommerce

--- ecommerceApplication/src/main/java/com/ttn/ecommerceApplication/ecommerceApplication/controller/CustomerController.java
+++ ecommerceApplication/src/main/java/com/ttn/ecommerceApplication/ecommerceApplication/controller/CustomerController.java
@@ -1,19 +1,20 @@
 package com.ttn.ecommerceApplication.ecommerceApplication.controller;
 
 import com.ttn.ecommerceApplication.ecommerceApplication.dao.CustomerDao;
-import com.ttn.ecommerceApplication.ecommerceApplication.entities.Address;
+import com.ttn.ecommerceApplication.ecommerceApplication.dao.UploadDao;;
+import com.ttn.ecommerceApplication.ecommerceApplication.dto.ProfileDTO;
 import com.ttn.ecommerceApplication.ecommerceApplication.entities.Customer;
 import com.ttn.ecommerceApplication.ecommerceApplication.entities.Seller;
-import com.ttn.ecommerceApplication.ecommerceApplication.entities.User;
-import com.ttn.ecommerceApplication.ecommerceApplication.repository.AddressRepository;
 import com.ttn.ecommerceApplication.ecommerceApplication.repository.CustomerRepository;
-import com.ttn.ecommerceApplication.ecommerceApplication.repository.UserRepository;
 import com.ttn.ecommerceApplication.ecommerceApplication.utilities.GetCurrentUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -22,19 +23,26 @@ public class CustomerController
      @Autowired
      CustomerDao customerDao;
 
+     @Autowired
+     GetCurrentUser getCurrentUser;
+
+     @Autowired
+     CustomerRepository customerRepository;
+
+     @Autowired
+     UploadDao  uploadDao;
+
      @GetMapping("/detailsOfCustomer")
      public List<Object[]> getDetails()
      {
         List<Object[]> objects = customerDao.getCustomerDetails();
         return objects;
      }
-
      @PutMapping("/getSellerAccount")
      public String  getAnSellerAccount(@RequestBody Seller seller)
      {
         return customerDao.getAnSellerAccount(seller);
      }
-
      @Lazy
      @PutMapping("/editContact")
      public String  editContact(@RequestBody Customer customer)
@@ -57,5 +65,29 @@ public class CustomerController
      {
         return customerDao.cancelOrder(orderStatusId);
      }
-
+     @GetMapping("/viewProfile")
+     public List<Object[]> viewProfile(HttpServletRequest request) throws IOException
+     {
+          return customerDao.viewProfile();
+     }
+     @PostMapping("/uploadProfilePic")
+     public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException
+     {
+          String username = getCurrentUser.getUser();
+          Customer customer = customerRepository.findByUsername(username);
+          return uploadDao.uploadSingleImage(file,customer);
+     }
+     @GetMapping("/viewProfileImage")
+     public ResponseEntity<Object> viewProfileImage(HttpServletRequest request) throws IOException {
+          String username = getCurrentUser.getUser();
+          Customer customer = customerRepository.findByUsername(username);
+          String filename = customer.getId().toString();
+          System.out.println(filename);
+          return uploadDao.downloadImage(filename,request);
+     }
+     @PutMapping("/updateProfile")
+     public String updateProfile(@RequestBody ProfileDTO customer)
+     {
+        return customerDao.updateProfile(customer);
+     }
 }
