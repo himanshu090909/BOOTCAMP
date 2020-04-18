416aa0e7f80e6db5791d69e91fc679682dcf1098
himanshu090909 <himanshu.bhansali@tothenew.com>
2020-04-18 15:52:07 +0530

    Implemented account locking using event listener

---
+++ ecommerceApplication/src/main/java/com/ttn/ecommerceApplication/ecommerceApplication/entities/CustomEventListener.java
@@ -0,0 +1,78 @@
+package com.ttn.ecommerceApplication.ecommerceApplication.entities;
+
+import com.ttn.ecommerceApplication.ecommerceApplication.repository.UserAttemptsRepository;
+import com.ttn.ecommerceApplication.ecommerceApplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashMap;
+
+@Component
+public class CustomEventListener
+{
+
+    @Autowired
+    UserAttemptsRepository userAttemptsRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @EventListener
+    public void AuthenticationFailEvent(AuthenticationFailureBadCredentialsEvent event)
+    {
+        String username = event.getAuthentication().getPrincipal().toString();
+        Iterable<UserAttempts> userAttempts = userAttemptsRepository.findAll();
+        int count=0;
+        for (UserAttempts userAttempts1 : userAttempts)
+        {
+            if (userAttempts1.getEmail().equals(username))
+            {
+                if (userAttempts1.getAttempts()>=2)
+                {
+                    User user = userRepository.findByUsername(username);
+                    user.setAccountNonLocked(false);
+                    userRepository.save(user);
+                }
+                else {
+                    userAttempts1.setAttempts(userAttempts1.getAttempts() + 1);
+                }
+            }
+        }
+        if (count==0)
+        {
+            UserAttempts userAttempts1 = new UserAttempts();
+            User user = userRepository.findByUsername(username);
+            userAttempts1.setEmail(user.getUsername());
+            userAttempts1.setAttempts(1);
+            userAttemptsRepository.save(userAttempts1);
+        }
+    }
+
+    @EventListener
+    public void AuthenticationPass(AuthenticationSuccessEvent event)
+    {
+        LinkedHashMap<String ,String > hashMap = (LinkedHashMap<String, String>) event.getAuthentication().getDetails();
+        Iterable<UserAttempts> userAttempts = userAttemptsRepository.findAll();
+       try {
+
+           for (UserAttempts userAttempts1 : userAttempts)
+           {
+               if (userAttempts1.getEmail().equals(hashMap.get("username")))
+               {
+                      userAttempts1.setAttempts(0);
+                      userAttemptsRepository.save(userAttempts1);
+               }
+           }
+       }
+       catch (Exception e)
+       {
+
+       }
+    }
+
+
+
+}
