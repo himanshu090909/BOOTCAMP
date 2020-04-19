2d82183717736b0ee12ec61059714b6053a74dce
himanshu090909 <himanshu.bhansali@tothenew.com>
2020-04-19 17:31:42 +0530

    event listener working properly now

--- ecommerceApplication/src/main/java/com/ttn/ecommerceApplication/ecommerceApplication/entities/CustomEventListener.java
+++ ecommerceApplication/src/main/java/com/ttn/ecommerceApplication/ecommerceApplication/entities/CustomEventListener.java
@@ -1,20 +1,27 @@
 package com.ttn.ecommerceApplication.ecommerceApplication.entities;
 import com.ttn.ecommerceApplication.ecommerceApplication.repository.UserAttemptsRepository;
 import com.ttn.ecommerceApplication.ecommerceApplication.repository.UserRepository;
+import com.ttn.ecommerceApplication.ecommerceApplication.utilities.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
+import java.util.LinkedHashMap;
 
 @Component
 public class CustomEventListener
 {
-
     @Autowired
     UserAttemptsRepository userAttemptsRepository;
 
     @Autowired
     UserRepository userRepository;
+
+    @Lazy
+    @Autowired
+    NotificationService notificationService;
 
     @EventListener
     public void AuthenticationFailEvent(AuthenticationFailureBadCredentialsEvent event)
@@ -31,9 +38,13 @@ public class CustomEventListener
                     User user = userRepository.findByUsername(username);
                     user.setAccountNonLocked(false);
                     userRepository.save(user);
+                    count++;
+                    notificationService.sendToSeller(user,"Regarding account","your account has been locked");
                 }
                 else {
                     userAttempts1.setAttempts(userAttempts1.getAttempts() + 1);
+                    userAttemptsRepository.save(userAttempts1);
+                    count++;
                 }
             }
         }
@@ -43,22 +54,23 @@ public class CustomEventListener
             User user = userRepository.findByUsername(username);
             userAttempts1.setEmail(user.getUsername());
             userAttempts1.setAttempts(1);
+            userAttemptsRepository.save(userAttempts1);
         }
     }
 
-   /* @EventListener
+   @EventListener
     public void AuthenticationPass(AuthenticationSuccessEvent event)
     {
-        LinkedHashMap<String ,String > hashMap = (LinkedHashMap<String, String>) event.getAuthentication().getDetails();
-   *//*     Iterable<UserAttempts> userAttempts = userAttemptsRepository.findAll();
        try {
+           LinkedHashMap<String ,String > hashMap = (LinkedHashMap<String, String>) event.getAuthentication().getDetails();
+           Iterable<UserAttempts> userAttempts = userAttemptsRepository.findAll();
+
 
            for (UserAttempts userAttempts1 : userAttempts)
            {
                if (userAttempts1.getEmail().equals(hashMap.get("username")))
                {
-                      userAttempts1.setAttempts(0);
-                      userAttemptsRepository.save(userAttempts1);
+                      userAttemptsRepository.deleteById(userAttempts1.getId());
                }
            }
        }
@@ -66,13 +78,5 @@ public class CustomEventListener
        {
 
        }
-    }*/
-
-
-
-
-
-
-
-
+    }
 }
