27c97771004390155dff61079ce07830865dcc73
himanshu090909 <60640645+himanshu090909@users.noreply.github.com>
2020-04-22 14:50:11 +0530

    Delete SBI.java

--- java/SBI.java
+++
@@ -1,9 +0,0 @@
-public class SBI implements Bank {
-    @Override
-    public void getDetails()
-    {
-        System.out.println("rate of interest is 6%");
-        System.out.println("education loan available");
-
-    }
-}
